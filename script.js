fde79ae64bf8b9ac7ff1eb814b58b3c89ae4b068
ohnoruu <rubydao007@gmail.com>
2024-11-08 17:09:21 -0800

    btedfc

---
+++ script.js
@@ -0,0 +1,19 @@
+const credits = 0;
+const credPerClick = 1;
+const highestUpgrade = 0;
+
+const upgrade1Purchased = false;
+const upgrade2Purchased = false;
+const upgrade3Purchase = false;
+
+function upgrade(upgradeNum){
+    if (upgradeNum == 1){
+
+    }
+    if (upgradeNum == 2){
+        
+    }
+    if (upgradeNum == 3){
+        
+    }
+}
